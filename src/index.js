bee32691d3356e6e098b064b321ec3b7425ac1d3
Raj Kumar <interplaytheheads@gmail.com>
2020-11-17 14:47:58 +0200

    Refactored the single into multiple components

--- src/index.js
+++ src/index.js
@@ -1,17 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const Header = (props) => {
+  return(
+    <h1>{props.course}</h1>
+  )
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Content = (props) => {
+  return(
+    <>
+    <p>{props.part1} {props.exercise1}</p>
+    <p>{props.part2} {props.exercise2}</p>
+    <p>{props.part3} {props.exercise3}</p>
+    </>
+  )
+}
+
+const Total = (props) => {
+  return(
+    <div>
+    <p>Number of Exercises: {props.exercise1 + props.exercise2 + props.exercise3}.</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack development Course';
+  const part1 = 'Fundamentals of React and Javacsript Programming';
+  const exercise1 = 10;
+  const part2 = 'Using props to pass the data';
+  const exercise2 = 7;
+  const part3 = 'State of the Component';
+  const exercise3 = 14;
+  
+  return (
+    <div>
+      <Header course={course} />
+      <Content part1 = {part1} part2 = {part2} part3 = {part3} exercise1 = {exercise1} exercise2 ={exercise2} exercise3 = {exercise3} />
+      <Total exercise1 = {exercise1} exercise2 ={exercise2} exercise3 = {exercise3}/>
+     </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
