7b5997182be2f81d35cef769be72d848fb15577b
Raj Kumar <interplaytheheads@gmail.com>
2020-11-22 00:42:07 +0200

    exercise1-5 completed

--- src/index.js
+++ src/index.js
@@ -1,34 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
+//Header
 const Header = (props) => {
   console.log(props);
   return(
     <h1>{props.course}</h1>
   )
 }
-
+//Content
 const Content = (props) => {
+  const parts = props.parts;
+  console.log(parts);
   return(
     <>
-    <p>{props.part1} {props.exercise1}</p>
-    <p>{props.part2} {props.exercise2}</p>
-    <p>{props.part3} {props.exercise3}</p>
+    <p>{parts[0].name} {parts[0].exercises}</p>
+    <p>{parts[1].name} {parts[1].exercises}</p>
+    <p>{parts[2].name} {parts[2].exercises}</p>
     </>
   )
 }
 
+
+//total
+
 const Total = (props) => {
+  const parts = props.parts;
   return(
     <div>
-    <p>Number of Exercises: {props.exercise1 + props.exercise2 + props.exercise3}.</p>
+    <p>Number of Exercises: {parts[0].exercises + parts[1].exercises + parts[2].exercises}.</p>
     </div>
   )
 }
 
+//App
 const App = () => {
-  const course = 'Half Stack development Course';
-  const parts = [
+  const course = {
+    name: 'Half Stack development Course',
+    parts: [
     {
       name: 'Fundamentals of React and Javacsript Programming',
       exercises: 10,
@@ -42,11 +52,12 @@ const App = () => {
       exercises: 14,
     }
   ]  
+}
   return (
     <>
-      <Header course={course} />
-      <Content part1 = {parts[0].name} part2 = {parts[1].name} part3 = {parts[2].name} exercise1 = {parts[0].exercises} exercise2 ={parts[1].exercises} exercise3 = {parts[2].exercises} />
-      <Total exercise1 = {parts[0].exercises} exercise2 ={parts[1].exercises} exercise3 = {parts[2].exercises}/>
+      <Header course={course.name} />
+      <Content parts={course.parts}  />
+      <Total parts={course.parts}/>
      </>
   )
 }
